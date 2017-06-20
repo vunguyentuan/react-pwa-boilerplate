27e8489eddac660776a3d11fde1a12f4b2df2a11
Vu Nguyen <tuanvu.vn007@gmail.com>
2017-06-20 20:21:36 +0700

    enable ModuleConcatenationPlugin

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -247,6 +247,7 @@ module.exports = {
     // It is absolutely essential that NODE_ENV was set to production here.
     // Otherwise React will be compiled in the very slow development mode.
     new webpack.DefinePlugin(env.stringified),
+    new webpack.optimize.ModuleConcatenationPlugin(),
     // Minify the code.
     new webpack.optimize.UglifyJsPlugin({
       compress: {
