115b0cf9ea8fd64284bd4c0c5103718f5f6c4a23
Vu Nguyen <tuanvu.vn007@gmail.com>
2017-06-20 20:29:27 +0700

    add react-helmet

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Helmet>
+            <meta charSet="utf-8" />
+            <title>Welcome to React</title>
+        </Helmet>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
